145bffd75d8137f5e7f9c6ae25e4c22b26ac5b90
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2023-06-01 12:54:02 -0300

    refactor(projetos/page)

--- src/routes/projetos/+page.svelte
+++ src/routes/projetos/+page.svelte
@@ -10,12 +10,19 @@
 	});
 
 	export let data: PageData;
-	const repos = data.repos.filter((e) => e.name !== 'portifolio' && e.name !== 'realfakenerd');
+	const filteredRepos = data.repos.filter((e) => e.name !== 'portifolio' && e.name !== 'realfakenerd');
 </script>
 
 <svelte:head>
 	<title>Projetos</title>
+	<meta name="description" content="Confira meus projetos recentes e seus detalhes.">
+	<meta name="keywords" content="projetos, GitHub, código-fonte, desenvolvimento, programação">
+	
+	<!-- Meta tags para compartilhamento em mídias sociais -->
+	<meta property="og:title" content="Projetos">
+	<meta property="og:description" content="Confira meus projetos recentes e seus detalhes.">
 </svelte:head>
+
 
 <Hero>
 	<div class="w-full">
@@ -23,23 +30,23 @@
 	</div>
 	<section class="w-full">
 		<ul class="grid gap-2">
-			{#each repos as repo (repo.id)}
+			{#each filteredRepos as repo (repo.id)}
 				<li
 					class="interactive-bg-background text-on-background flex w-full flex-col justify-between gap-2 self-stretch rounded-xl p-4"
 				>
 					<section class="flex flex-col gap-y-2">
-						<h2 class="text-title-large capitalize" aria-label={repo.name}>
+						<h2 class="text-title-large capitalize" aria-label="Nome do repositório: {repo.name}">
 							<span>{repo.name}</span>
 						</h2>
-						<div class="flex flex-row gap-2">
 							{#if repo.language}
-								<span
-									class="ring-outline text-label-small inline-flex place-items-center rounded-md px-4 ring-1"
-								>
-									{repo.language}
-								</span>
+								<div class="flex flex-row gap-2">
+									<span
+										class="ring-outline text-label-small inline-flex place-items-center rounded-md px-4 ring-1"
+									>
+										{repo.language}
+									</span>
+								</div>
 							{/if}
-						</div>
 						<p class="text-body-medium">{repo.description}</p>
 					</section>
 					<div class="flex flex-row gap-2">
@@ -47,7 +54,7 @@
 							class="button fill-on-primary interactive-bg-primary text-on-primary"
 							rel="noreferrer"
 							href={repo.html_url}
-							aria-label="github url"
+							aria-label="URL do repositório no GitHub"
 							target="_blank"
 						>
 							<Icon
@@ -62,7 +69,7 @@
 								class="button fill-on-secondary interactive-bg-secondary text-on-secondary"
 								rel="noreferrer"
 								href={repo.homepage}
-								aria-label="homepage url"
+								aria-label="URL da página inicial"
 								target="_blank"
 							>
 								<Icon
