a78081fce54348faa91e33b2c72b69aa4e41f818
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2023-06-01 11:14:17 -0300

    refactor(Textfield)

--- src/lib/components/TextField.svelte
+++ src/lib/components/TextField.svelte
@@ -107,71 +107,64 @@
 	.text-field-container {
 		@apply text-on-surface-variant relative h-14 w-full min-w-[15rem];
 	}
-
 	.text-field-container :global(svg) {
 		@apply h-6 w-6;
 	}
-
 	.text-field-input {
 		@apply text-on-surface absolute inset-0 h-full w-full border-none bg-transparent px-4 outline-none;
 	}
-
 	textarea {
 		@apply resize-none;
 	}
-
 	.text-field-layer {
 		@apply pointer-events-none absolute inset-0 rounded-[inherit] transition-all;
 	}
-
 	label {
 		transition: all 250ms, font 500ms;
 		transition-timing-function: cubic-bezier(0.254, 0.029, 0, 1.2);
 		color: rgb(var(--error, currentColor));
 		@apply pointer-events-none absolute left-4 top-4 ease-in-out;
 	}
-
 	.error {
 		--error: var(--color-error);
 	}
-
 	.supporting {
 		@apply text-on-surface-variant text-label-small mt-1 px-4;
 	}
-
 	.supporting.error {
 		@apply text-error;
 	}
 
-	.text-field-container.has-icon > .text-field-input {
-		@apply pl-[3.25rem];
+	.style-filled {
+		@apply bg-surface-variant rounded-t-lg;
+	}
+	.style-filled > .text-field-layer {
+		color: rgb(var(--error, var(--color-on-surface-variant)));
+		@apply border-b-2 border-[currentColor];
+	}
+	.style-filled > .text-field-input {
+		@apply pb-2 pt-6;
+	}
+	.style-filled .text-field-input:is(:focus, .value, :required:valid, [type='date']) ~ label {
+		@apply top-2;
 	}
 
-	.text-field-container.has-icon > label {
-		@apply left-[3.25rem];
-	}
-
-	.text-field-container.has-trailing-icon > .text-field-input {
-		@apply pr-[3.25rem];
-	}
-
-	.style-filled,
 	.style-outlined {
 		@apply rounded-lg;
 	}
-
-	.style-filled > .text-field-layer,
 	.style-outlined > .text-field-layer {
 		color: rgb(var(--error, var(--color-outline)));
+		@apply ring-1 ring-[currentColor];
 	}
-
-	.style-filled > .text-field-input,
 	.style-outlined > .text-field-input {
 		@apply py-4;
 	}
-
-	.style-filled .text-field-input:is(:focus, .value, :required:valid, [type='date']) ~ label,
 	.style-outlined .text-field-input:is(:focus, .value, :required:valid, [type='date']) ~ label {
+		background-color: rgb(var(--color-surface));
+		@apply -top-2 left-3 px-1;
+	}
+
+	.text-field-input:is(:focus, .value, :required:valid, [type='date']) ~ label {
 		@apply text-label-small;
 	}
 
@@ -180,40 +173,45 @@
 		fill: rgb(var(--error, var(--color-on-surface)));
 		@apply pointer-events-none inline-flex self-center;
 	}
-
 	.leading-icon {
 		@apply ml-4 mr-3;
 	}
-
 	.trailing-icon {
 		margin: 0 0.75rem 0 auto;
 		fill: rgb(var(--error));
 	}
-
 	.trailing-button {
 		fill: rgb(var(--error, var(--color-primary)));
 		@apply text-on-surface-variant absolute bottom-0 right-0 top-0 inline-flex w-[3.25rem] cursor-pointer items-center 
 		justify-center border-none bg-transparent transition-all duration-200;
 	}
-
 	.trailing-button:is(:focus-visible, :active) {
 		background-color: rgb(var(--color-on-surface-variant) / 0.12);
 	}
+	.has-icon > .text-field-input {
+		@apply pl-[3.25rem];
+	}
+	.has-icon > label {
+		@apply left-[3.25rem];
+	}
+	.has-trailing-icon > .text-field-input {
+		@apply pr-[3.25rem];
+	}
 
-	@media(hover:hover) {
+	@media (hover: hover) {
+		.error:hover {
+			--error: var(--color-on-error-container);
+		}
 		.text-field-container:hover > :is(label, .text-field-layer) {
-		color: rgb(var(--error, var(--color-on-surface)));
+			color: rgb(var(--error, var(--color-on-surface)));
+		}
+		.style-filled:hover > .text-field-layer {
+			background-color: rgb(var(--color-on-surface) / 0.08);
+		}
+		.trailing-button:hover {
+			background-color: rgb(var(--color-on-surface-variant) / 0.08);
+		}
 	}
-
-	.style-filled:hover > .text-field-layer {
-		background-color: rgb(var(--color-on-surface) / 0.08);
-	}
-
-	.trailing-button:hover {
-		background-color: rgb(var(--color-on-surface-variant) / 0.08);
-	}
-	}
-
 	.text-field-container:focus-within > :is(label, .text-field-layer) {
 		color: rgb(var(--error, var(--color-primary)));
 	}
@@ -225,36 +223,40 @@
 	.text-field-input[type='date'] {
 		@apply pl-[0.875rem];
 	}
-
 	.has-icon > .text-field-input[type='date'] {
 		@apply pl-[3.25rem];
 	}
-
 	@supports (-moz-appearance: none) {
 		.text-field-input[type='date'] {
 			@apply pl-3;
 		}
-
 		.has-icon > .text-field-input[type='date'] {
 			@apply pl-12;
 		}
 	}
-
 	.has-trailing-icon.text-field-input[type='date'] {
 		@apply pr-4;
 	}
-
-	.text-field-container.has-trailing-icon.text-field-input[type='date'] {
-		padding-right: 0;
+	.text-field-input[type='date'] ~ .trailingButton {
+		@apply hidden;
 	}
-
-	.text-field-container.has-trailing-icon.text-field-input[type='date']::-webkit-calendar-picker-indicator {
-		display: none;
-	}
-
-	@supports not selector(::-webkit-calendar-picker-indicator) {
-		.text-field-container.has-trailing-icon.text-field-input[type='date'] {
-			clip-path: inset(0 3.25rem 0 0);
+	@media (orientation: landscape) and (forced-colors: none) {
+		.text-field-input[type='date'] {
+			padding-right: 3.25rem;
+		}
+		.text-field-input[type='date'] ~ .trailingButton {
+			display: inline-flex;
+		}
+		.text-field-input[type='date']::-webkit-calendar-picker-indicator {
+			display: none;
+		}
+		@supports not selector(::-webkit-calendar-picker-indicator) {
+			.text-field-input[type='date'] {
+				clip-path: inset(0 3.25rem 0 0);
+			}
+			.has-trailing-icon.text-field-input[type='date'] {
+				padding-right: 0;
+			}
 		}
 	}
 
@@ -262,7 +264,6 @@
 		print-color-adjust: exact;
 		-webkit-print-color-adjust: exact;
 	}
-
 	@media screen and (forced-colors: active) {
 		.style-filled {
 			background-color: field;
