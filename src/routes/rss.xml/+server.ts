4ef5c25144ee0124128978417db78b706ae96b00
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2023-06-01 14:03:30 -0300

    fix(rss)

--- src/routes/rss.xml/+server.ts
+++ src/routes/rss.xml/+server.ts
@@ -35,7 +35,7 @@ export const GET: RequestHandler = async ({ fetch }) => {
               .join('')}
           </item>`).join('')}
       </channel>
-    </rss>`;
+    </rss>`.trim();
 
   return new Response(xml, { headers });
 };
