3db93f771dc9406c065ebb9884fc73b9a3a9a0c6
Lucas A. Ouverney <realfakenerd@users.noreply.github.com>
2023-09-15 18:37:10 -0300

    Update +server.ts

--- src/routes/api/posts/[slug]/+server.ts
+++ src/routes/api/posts/[slug]/+server.ts
@@ -1,1 +1,13 @@
-
+import { getPost } from '$lib/markdown/posts';
+import { error } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
+export const GFT = (async ({ params }) => {
+	try {
+		const post = await getPost(params.slug);
+		return json(post);
+	} catch (e) {
+		console.log(e);
+		
+		throw error(404, `Could not find ${params.articleId}`);
+	}
+});
