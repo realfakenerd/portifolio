44be084907b2462a39b6f96dbfd5637597046998
realfakenerd <lucas210898@hotmail.com>
2023-09-18 11:02:00 -0300

    try fix post #2

--- src/routes/blog/[articleId]/+page.server.ts
+++ src/routes/blog/[articleId]/+page.server.ts
@@ -1,9 +1,16 @@
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
 import { fetchJSON } from '$lib/markdown/posts';
+
+interface ContentType {
+	content: string;
+	fm: Fm;
+}
+
+
 export const load = (async ({ fetch, params }) => {
 	try {
-		const post = await fetchJSON(`/api/posts/${params.articleId}`, fetch);
+		const post = await fetchJSON<ContentType>(`/api/posts/${params.articleId}`, fetch);
 		return {
 			content: post.content,
 			meta: post.fm
