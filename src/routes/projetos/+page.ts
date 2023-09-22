5e41c05fa0ec67c2dbd931eb472c484fa005890c
realfakenerd <lucas210898@hotmail.com>
2023-09-22 12:20:37 -0300

    try fix prerender

--- src/routes/projetos/+page.ts
+++ src/routes/projetos/+page.ts
@@ -1,5 +1,6 @@
 import type { PageLoad } from './$types';
 
+export const prerender = true;
 export const load = (async ({ fetch }) => {
 	const res = await fetch('/api/git/repo');
 	const data = (await res.json()) as Repo[];
