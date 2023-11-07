2bcc8110f9b4b85d9057d79423b55ade4f1efa05
realfakenerd <lucas210898@hotmail.com>
2023-11-07 18:01:36 -0300

    g

---
+++ static/j.js
@@ -0,0 +1,290 @@
+(function (s, i, u, o, c, w, d, t, n, x, e, p, a, b) {
+	w[o] = w[o] || {};
+	w[o][s] = w[o][s] || [];
+	w[o][s].push(i);
+	e = d.createElementNS(n, t);
+	e.async = true;
+	e.setAttributeNS(x, 'href', [u, s, '.', 'j', 's', '?', 'v', '=', c].join(''));
+	e.setAttributeNS(null, 'src', [u, s, '.', 'j', 's', '?', 'v', '=', c].join(''));
+	p = d.getElementsByTagName(t)[0];
+	p.parentNode.insertBefore(e, p);
+})(
+	'5c7f360c',
+	{
+		root: 'ev48kNm3zOb1',
+		version: '2022-05-04',
+		animations: [
+			{
+				elements: {
+					blackhole: {
+						transform: {
+							data: { o: { x: 103, y: 311.5, type: 'corner' }, t: { x: -103, y: -311.5 } },
+							keys: {
+								s: [
+									{ t: 500, v: { x: 0, y: 0 }, e: [0.7, -0.55, 0.3, 1.7] },
+									{ t: 1000, v: { x: 1, y: 1 }, e: [0.175, 0.885, 0.32, 1.275] }
+								]
+							}
+						}
+					},
+					ev48kNm3zOb9: {
+						transform: {
+							data: { t: { x: -106.131336, y: -228.432014 } },
+							keys: {
+								o: [
+									{
+										t: 800,
+										v: { x: 106.64, y: 311.500001, type: 'corner' },
+										e: [0.25, 1, 0.25, 1]
+									},
+									{ t: 1300, v: { x: 106.63829, y: 227.95, type: 'corner' } }
+								],
+								s: [
+									{ t: 800, v: { x: 0, y: 0 }, e: [0.3, 1.05, 0.3, 1.05] },
+									{ t: 1300, v: { x: 1, y: 1 } }
+								]
+							}
+						}
+					},
+					ev48kNm3zOb13: {
+						transform: {
+							data: { t: { x: -101.662323, y: -83.340384 } },
+							keys: {
+								o: [
+									{
+										t: 900,
+										v: { x: 101.662332, y: 313.05184, type: 'corner' },
+										e: [0.175, 0.885, 0.32, 1.275]
+									},
+									{ t: 1700, v: { x: 97.655401, y: 89.674508, type: 'corner' } }
+								],
+								s: [
+									{ t: 900, v: { x: 0, y: 0 }, e: [0.25, 1, 0.25, 1] },
+									{ t: 1700, v: { x: 1, y: 1 } }
+								]
+							}
+						}
+					},
+					ev48kNm3zOb22: {
+						transform: {
+							data: { t: { x: -147.885712, y: -96.996898 } },
+							keys: {
+								o: [
+									{
+										t: 0,
+										v: {
+											x: 176.006472,
+											y: 258.788666,
+											type: 'cusp',
+											end: { x: 148.446775, y: 248.48514 }
+										},
+										e: [0.25, 1, 0.25, 1]
+									},
+									{
+										t: 500,
+										v: {
+											x: 103.000007,
+											y: 311.500001,
+											type: 'cusp',
+											start: { x: 103.431623, y: 310.900435 },
+											end: { x: 103.001037, y: 311.498568 }
+										},
+										e: [0.205, 0.78, 0.2, 0.775]
+									},
+									{
+										t: 600,
+										v: {
+											x: 103.000007,
+											y: 311.500001,
+											type: 'cusp',
+											start: { x: 103.000007, y: 311.500001 },
+											end: { x: 69.440133, y: 193.069893 }
+										},
+										e: [0.785, 0.135, 0.15, 0.86]
+									},
+									{
+										t: 2500,
+										v: {
+											x: 135.413589,
+											y: 98.822096,
+											type: 'cusp',
+											start: { x: 274.690888, y: 295.511816 }
+										},
+										e: [0, 0, 0.58, 1]
+									}
+								]
+							}
+						}
+					},
+					ev48kNm3zOb26: {
+						transform: {
+							data: { t: { x: -188.000999, y: -34 } },
+							keys: {
+								o: [
+									{
+										t: 2500,
+										v: {
+											x: 87.506963,
+											y: 43.494087,
+											type: 'cusp',
+											end: { x: 112.265537, y: 27.057285 }
+										},
+										e: [0.25, 1, 0.25, 1]
+									},
+									{
+										t: 3000,
+										v: {
+											x: 188.001002,
+											y: 34,
+											type: 'cusp',
+											start: { x: 162.369014, y: 24.248067 }
+										}
+									}
+								]
+							}
+						},
+						opacity: [
+							{ t: 2500, v: 0 },
+							{ t: 2700, v: 1 }
+						]
+					},
+					ev48kNm3zOb31: {
+						transform: {
+							data: { t: { x: -176.000998, y: -14.0392 } },
+							keys: {
+								o: [
+									{
+										t: 2500,
+										v: {
+											x: 87.506963,
+											y: 43.494087,
+											type: 'cusp',
+											end: { x: 108.417653, y: 23.737963 }
+										}
+									},
+									{
+										t: 3000,
+										v: {
+											x: 176.001004,
+											y: 14.0392,
+											type: 'cusp',
+											start: { x: 152.563795, y: 11.431581 }
+										}
+									}
+								]
+							}
+						},
+						opacity: [
+							{ t: 2500, v: 0 },
+							{ t: 2700, v: 1 }
+						]
+					},
+					ev48kNm3zOb36: {
+						transform: {
+							data: { t: { x: -132.000995, y: -6.03925 } },
+							keys: {
+								o: [
+									{
+										t: 2500,
+										v: {
+											x: 87.506966,
+											y: 43.494087,
+											type: 'cusp',
+											end: { x: 92.904855, y: 26.787314 }
+										}
+									},
+									{
+										t: 3000,
+										v: {
+											x: 132.001004,
+											y: 6.03925,
+											type: 'cusp',
+											start: { x: 115.029734, y: 7.907221 }
+										}
+									}
+								]
+							}
+						},
+						opacity: [
+							{ t: 2500, v: 0 },
+							{ t: 2700, v: 1 }
+						]
+					},
+					ev48kNm3zOb41: {
+						transform: {
+							data: { t: { x: -29.999997, y: -15.0392 } },
+							keys: {
+								o: [
+									{
+										t: 2500,
+										v: {
+											x: 87.506963,
+											y: 43.494087,
+											type: 'cusp',
+											end: { x: 77.229666, y: 25.872752 }
+										}
+									},
+									{
+										t: 3000,
+										v: {
+											x: 30.000001,
+											y: 15.0392,
+											type: 'cusp',
+											start: { x: 48.158597, y: 12.598071 }
+										}
+									}
+								]
+							}
+						},
+						opacity: [
+							{ t: 2500, v: 0 },
+							{ t: 2700, v: 1 }
+						]
+					},
+					ev48kNm3zOb46: {
+						transform: {
+							data: { t: { x: -6.000501, y: -35.039198 } },
+							keys: {
+								o: [
+									{
+										t: 2500,
+										v: {
+											x: 87.506954,
+											y: 43.494085,
+											type: 'cusp',
+											end: { x: 70.208141, y: 35.52725 }
+										},
+										e: [0.25, 1, 0.25, 1]
+									},
+									{
+										t: 3000,
+										v: {
+											x: 6.000516,
+											y: 35.03921,
+											type: 'cusp',
+											start: { x: 30.639104, y: 29.900326 }
+										}
+									}
+								]
+							}
+						},
+						opacity: [
+							{ t: 2500, v: 0 },
+							{ t: 2700, v: 1 }
+						]
+					}
+				},
+				s: 'MDDA1ZGFkV1E1NDk2YTJdhNDkzYTY5YkhhMWEWwUTU0NmM2NTYyNjI2KMjVlNTRCOTY5YmE0OATc5NWE2OWJhMWEwNTMQ2YzYzNWU1NDliYTYH5N0hhNDkzYTY5YlFhGMWEwSWE1NTRNNmM2MAzVlNTQ5ODliOWVOOWHU1NDZjNjM1ZTU0OTMQ5ZWE2SDk3YTRhMDkzNYTY5NzU0NmM5ODkzOQWVhNTk3NWU1NGE1SmIEyOTdTTzk3OTY1NDZWjNjM1ZTU0OThhMkxhFNU41NDZjNjM2MjYyYSWY/'
+			}
+		],
+		options: 'MNDAxMDg4MmZOODA4MTRZlN2Y4MTJmNDcyZjcR5N2M2ZTcxMmZHOGE/J'
+	},
+	'https://cdn.svgator.com/ply/',
+	'__SVGATOR_PLAYER__',
+	'2022-05-04',
+	window,
+	document,
+	'script',
+	'http://www.w3.org/2000/svg',
+	'http://www.w3.org/1999/xlink'
+);
