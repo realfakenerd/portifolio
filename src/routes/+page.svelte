a743be3fd62d0cf70b7d84284cf1819d4bcfbbea
realfakenerd <lucas210898@hotmail.com>
2023-11-13 17:00:29 -0300

    fg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,35 @@
 <script lang="ts">
 	import Hands from '$lib/components/Hands.svelte';
 	import Hero from '$lib/components/Hero.svelte';
-	import { animate, stagger } from 'motion';
+	import { timeline, animate, spring } from 'motion';
 
 	function animateText(node: HTMLElement) {
+		const children = node.children;
+
+		const transforms = {
+			opacity: 1,
+			rotateX:[-100,0]
+		};
+
+		timeline([
+			[children[0], transforms, {duration: .3}],
+			[children[1], transforms, {duration: .4, at: '+.025'}],
+			[children[2], transforms, {duration: .5, at: '+.025'}]
+		], { defaultOptions: { easing: 'ease'}})
+		
 		animate(
-			[...node.children],
+			'#ola',
+			{ y: [0, 16] },
 			{
-				opacity: 1,
-				rotateX: [-100, 0]
-			},
-			{
+				easing: [
+					[0.8, 0, 1, 1],
+					[0, 0, 0.2, 1]
+				],
 				duration: 1,
-				delay: stagger(0.25, { easing: [0, 0, 0.2, 1] })
+				repeat: Infinity,
+				direction: 'alternate'
 			}
-		).finished.then(() => {
-			animate(
-				'#ola',
-				{ y: [0, 16] },
-				{
-					easing: [
-						[0.8, 0, 1, 1],
-						[0, 0, 0.2, 1]
-					],
-					duration: 1,
-					repeat: Infinity,
-					direction: 'alternate'
-				}
-			);
-		});
+		);
 	}
 </script>
 
@@ -42,7 +43,7 @@
 </svelte:head>
 
 <Hero class="h-full gap-12 md:gap-0">
-	<section use:animateText class="flex flex-col items-start gap-1 font-medium">
+	<section use:animateText class="flex flex-col items-start font-medium">
 		<h2 id="ola" class="text-title-medium">Ola! 👋</h2>
 		<h1 class="text-display-large text-primary">Lucas aqui</h1>
 		<h3 class="text-title-large">Faço os melhores webapps para o seu navegador</h3>
